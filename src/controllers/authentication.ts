1df56ffdb958a809e5ffc1a4696ccb447ec52705
Vadym Kapinos <kap.vadym@gmail.com>
2026-01-27 23:28:20 +0100

    Change API with catchAsync and ErrorMiddleware

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,70 +1,64 @@
-import express from "express";
-
+import { Request, Response } from "express";
 import { createUser, getUserByEmail } from "../db/users";
 import { authentication, random } from "../helpers";
 
-export const login = async(req:express.Request, res: express.Response) => {
-  try {
-    const {email, password} = req.body;
+import { ApiError } from "../errors/ApiError";
+import { catchAsync } from "../middlewares/catchAsync";
 
-    if(!email || !password) {
-      return res.sendStatus(400);
-    }
+export const login = catchAsync(async (req: Request, res: Response) => {
+  const { email, password } = req.body;
 
-    const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
-
-    if(!user){
-      return res.sendStatus(400);
-    }
-
-    const expectedHash = authentication(user.authentication.salt, password);
-
-    if(user.authentication.password !== expectedHash){
-      return res.sendStatus(403);
-    }
-
-    const salt = random();
-    user.authentication.sessionToken = authentication(salt, user._id.toString());
-    await user.save()
-
-    res.cookie('USER-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
-
-    return res.status(200).json(user).end();
-
-  } catch (error) {
-    console.log(error)
-    return res.sendStatus(400);
+  if(!email || !password) {
+    throw new ApiError(400, "Email and password are required");
   }
-}
 
-export const register = async(req: express.Request, res: express.Response) => {
-  try {
-    const { email, password, username } = req.body;
+  const user = await getUserByEmail(email)
+    .select('+authentication.salt +authentication.password');
 
-    if(!email || !password || !username) {
-      return res.sendStatus(400);
-    }
-
-    const existingUser = await getUserByEmail(email);
-
-    if(existingUser) {
-      return res.sendStatus(400)
-    }
-    
-    const salt = random();
-    const user = await createUser({
-      email,
-      username,
-      authentication: {
-        salt,
-        password: authentication(salt, password)
-      },
-    });
-
-    return res.status(200).json(user).end();
-
-  } catch(error) {
-    console.log(error);
-    return res.sendStatus(400);
+  if(!user){
+    throw new ApiError(400, "Invalid email or password");
   }
-}
+
+  const expectedHash = authentication(user.authentication.salt, password);
+
+  if(user.authentication.password !== expectedHash){
+    throw new ApiError(403, "Invalid email or password");
+  }
+
+  const salt = random();
+  user.authentication.sessionToken = authentication(salt, user._id.toString());
+  await user.save()
+
+  res.cookie('USER-AUTH', user.authentication.sessionToken, { 
+    domain: 'localhost', 
+    path: '/' 
+  });
+
+  res.status(200).json(user);
+});
+
+export const register = catchAsync(async (req: Request, res: Response) => {
+  const { email, password, username } = req.body;
+
+  if(!email || !password || !username) {
+    throw new ApiError(400, "Email, password and username are required");
+  }
+
+  const existingUser = await getUserByEmail(email);
+
+  if(existingUser) {
+    throw new ApiError(409, "User already exists");
+  }
+  
+  const salt = random();
+  const user = await createUser({
+    email,
+    username,
+    authentication: {
+      salt,
+      password: authentication(salt, password)
+    },
+  });
+
+  res.status(200).json(user);
+});
--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,66 +1,53 @@
-import express from 'express';
+import { Request, Response } from 'express';
 import { deleteUserById, getUserById, getUsers } from '../db/users';
 
-export const getUser = async(req: express.Request, res: express.Response) => {
-  try {
-    const id = String(req.params.id);
+import { catchAsync } from '../middlewares/catchAsync';
+import { ApiError } from '../errors/ApiError';
 
-    if(!id) {
-      return res.sendStatus(403);
-    }
+export const getUser = catchAsync(async (req: Request, res: Response) => {
+  const id = String(req.params.id);
 
-    const user = await getUserById(id);
-    if(!user) {
-      return res.sendStatus(403);
-    }
-
-    return res.status(200).json(user).end();
-  } catch (error) {
-    console.log(error);
-    return res.sendStatus(400);
+  if(!id) {
+    throw new ApiError(403, "Invalid id");
   }
+
+  const user = await getUserById(id);
+  if(!user) {
+    throw new ApiError(403, "Invalid user");
+  }
+
+  res.status(200).json(user);
+})
+
+export const getAllUsers = catchAsync(async (req: Request, res: Response) => {
+  const users = await getUsers();
+
+  if(!users) {
+    throw new ApiError(404, "Users not found")
+  }
+
+  res.status(200).json(users);
+})
+
+export const deleteUser = async(req: Request, res: Response) => {
+  const id = String(req.params.id);
+
+  const deletedUser = await deleteUserById(id);
+
+  res.json(deletedUser);
 }
 
-export const getAllUsers = async(req: express.Request, res:express.Response) => {
-  try {
-    const users = await getUsers();
+export const updateUser = async(req: Request, res: Response) => {
+  const id = String(req.params.id);
+  const { username } = req.body;
 
-    return res.status(200).json(users).end();
-  } catch (error) {
-    console.log(error)
-    return res.sendStatus(400)
+  if(!username) {
+    throw new ApiError(400, "Invalid username");
   }
-}
 
-export const deleteUser = async(req: express.Request, res:express.Response) => {
-  try {
-    const id = String(req.params.id);
+  const user = await getUserById(id);
+  user.username = username
+  await user.save()
 
-    const deletedUser = await deleteUserById(id);
-
-    return res.json(deletedUser);
-  } catch (error) {
-    console.log(error);
-    return res.sendStatus(400)
-  }
-}
-
-export const updateUser = async(req:express.Request, res:express.Response) => {
-  try {
-    const id = String(req.params.id);
-    const { username } = req.body;
-
-    if(!username){
-      return res.sendStatus(400);
-    }
-
-    const user = await getUserById(id);
-    user.username = username
-    await user.save()
-
-    return res.status(200).json(user).end();
-  } catch (error) {
-    console.log(error)
-    return res.sendStatus(400)
-  }
+  res.status(200).json(user);
 }
